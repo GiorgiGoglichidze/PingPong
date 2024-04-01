b31b77fbccd8e9c14a0e30b41189faff10c76210
GiorgiGoglichidze <165707545+GiorgiGoglichidze@users.noreply.github.com>
2024-04-01 20:42:03 +0200

    Create pingpong_players.py

---
+++ pingpong_players.py
@@ -0,0 +1,24 @@
+import turtle
+import random
+
+
+class Player(turtle.Turtle):
+    def __init__(self,xxcor):
+        super().__init__()
+        self.xxcor = xxcor
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(self.xxcor, 0)
+
+    def change_up(self):
+        if self.ycor() + 20 < 250:
+            self.goto(self.xcor(), self.ycor() + 20)
+
+    def change_down(self):
+        if self.ycor() - 20 > -250:
+            self.goto(self.xcor(), self.ycor() - 20)
+
+    def change_Ai(self, piv):
+        self.goto(self.xcor(), self.ycor() + (random.randint(1, 25) * piv))
